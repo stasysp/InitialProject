bb1496326e1cc74aef6676af8165ef5d4832416e
stasysp <stasysp.96@gmail.com>
2017-11-22 01:25:31 +0300

    fix #include<vector>
    the same reason https://github.com/stasysp/InitialProject/pull/1

---
+++ MainListener.hpp
@@ -0,0 +1,14 @@
+#ifndef HIGHLOADCUP_MAINLISTENER_H
+#define HIGHLOADCUP_MAINLISTENER_H
+
+#include <Poco/Util/ServerApplication.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+class MainListener final : public Poco::Util::ServerApplication {
+    virtual int main(const std::vector<std::string>& args) override;
+};
+
+
+#endif // HIGHLOADCUP_MAINLISTENER_H
